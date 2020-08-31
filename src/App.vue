c09cc616eef0c96f77dc22b1e9b3c5a15e95b755
IntegerVector <andrew.ryban@gmail.com>
2020-09-01 02:35:21 +0300

    base components initialization

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,92 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Work in progress"/>
-  </div>
+  <main class="main-container">
+    <TasksList
+      class="task-list-container"
+      v-bind:tasks="mockData.tasks">
+    </TasksList>
+    <AddTaskListItem
+      class="add-task-list-item-container">
+    </AddTaskListItem>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TasksList from './components/TasksList.vue';
+import AddTaskListItem from './components/AddTaskListItem.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TasksList,
+    AddTaskListItem
+  },
+  data() {
+    return {
+      mockData: {
+        tasks: [
+          {
+            id: 1,
+            text: 'Task №1'
+          },
+          {
+            id: 2,
+            text: 'Task №2'
+          },
+          {
+            id: 3,
+            text: 'Task №3'
+          },
+          {
+            id: 4,
+            text: 'Task №4'
+          },
+          {
+            id: 5,
+            text: 'Task №5'
+          },
+        ]
+      }
+    };
   }
 }
 </script>
 
 <style>
-#app {
+:root {
+  font-size: x-large;
+  background-color: white;
+}
+
+* {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+}
+
+html, body, main {
+  width: 100%;
+  height: 100%;
+}
+
+.main-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+
+.task-list-container {
+  flex-grow: 1;
+}
+
+.add-task-list-item-container {
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+  width: 3rem;
+  height: 3rem;
 }
 </style>
---
+++ src/components/AddTaskListItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="add-task-section-container">
+    ＋
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AddTaskListItem'
+}
+</script>
+
+<style scoped>
+.add-task-section-container{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 2rem;
+  user-select: none;
+  outline: none;
+  color: white;
+  border-radius: 50%;
+  border: 1px solid transparent;
+  border-radius: 50%;
+  background-color: #568bff;
+  box-shadow: 0 0.1rem 0.2rem 0 rgba(0, 0, 0, 0.8);
+  -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+  cursor: pointer;
+}
+
+.add-task-section-container:hover {
+  background-color: #3e6bff;
+  transition: 400ms;
+}
+
+.add-task-section-container:active {
+  background-color: #0000ff;
+}
+</style>
