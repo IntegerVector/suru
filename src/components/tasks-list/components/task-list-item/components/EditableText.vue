e4fed9419d9859a0c3efdb7d10d841e6ce88f994
IntegerVector <andrew.ryban@gmail.com>
2020-10-14 16:47:21 +0300

    Editable text component added

--- src/components/tasks-list/components/task-list-item/components/EditableText.vue
+++ src/components/tasks-list/components/task-list-item/components/EditableText.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="editable-text">
     <input
+      id="text-input"
       class="editable-text__input"
-      v-if="mode === 'edit'"
-      type="text">
-    <span
-      class="editable-text__lable"
-      v-if="mode === 'view'"
-      :title="text">
-      {{ text }}
-    </span>
+      type="text"
+      v-model="text"
+      @input="emitChanges">
   </div>
 </template>
 
@@ -17,22 +13,39 @@
 export default {
   name: 'EditableText',
   props: {
-    mode: String,
-    text: String
+    modelValue: String
+  },
+  data() {
+    return {
+      text: this.modelValue
+    };
+  },
+  methods: {
+    emitChanges() {
+      this.$emit('update:modelValue', this.text);
+    }
   }
 }
 </script>
 
 <style scoped>
 .editable-text {
-
-}
-
-.editable-text__lable {
-
+  display: flex;
+  align-items: center;
 }
 
 .editable-text__input {
+  flex-grow: 1;
+  font-size: var(--text-size--regular);
+  font-family: var(--text-font--regular);
+  height: 1rem;
+  border-radius: 0.2rem;
+  border: 0 solid transparent;
+}
 
+.editable-text__input:focus {
+  outline: none;
+  transition: var(--focus-speed);
+  border: 0.2rem solid var(--focus-color);
 }
 </style>
